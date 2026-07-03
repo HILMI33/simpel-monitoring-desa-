c17bfdc7232365267616992e9072a831eb36f8ac
HILMI33 <kiwilxyzz414@gmail.com>
2026-07-03 10:04:15 +0700

    Final Project

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -15,7 +15,6 @@ abstract class Routes {
   static const UPDATE_PROGRES = '/update-progres';
   static const DETAIL_REPORT = '/detail-report';
   static const DETAIL_PEMBANGUNAN = '/detail-pembangunan';
-  static const EMAIL_VERIFICATION = '/email-verification';
   static const ACCOUNT_DETAILS = '/account-details';
   static const EDIT_PROFILE = '/edit-profile';
   static const ACTIVITY_LOG = '/activity-log';
